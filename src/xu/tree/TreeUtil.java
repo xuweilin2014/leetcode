b04ffaf17e2f7514659f787ac68618e34681f84d
纬林 许 <xuweilin2014@outlook.com>
2020-01-29 18:52:33 +0800

    解决树问题：654、701、938、1302、1315

---
+++ src/xu/tree/TreeUtil.java
@@ -0,0 +1,53 @@
+package xu.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeUtil {
+
+    public static TreeNode buildTree(Integer[] vals){
+        int length = vals.length;
+        if (length == 0)
+            return null;
+
+        TreeNode root = new TreeNode(vals[0]);
+
+        if (length == 1)
+            return root;
+
+        Queue<TreeNode> treeNodes = new LinkedList<>();
+        int nextLayerIndex = 1;
+        treeNodes.add(root);
+
+        while (nextLayerIndex < length){
+            TreeNode node = treeNodes.poll();
+            for (int i = 1; i <= 2; i++){
+                Integer val = vals[nextLayerIndex++];
+                TreeNode anode;
+                if (val == null){
+                    anode = null;
+                }else{
+                    anode = new TreeNode(val);
+                    anode.right = null;
+                    anode.left = null;
+                    treeNodes.add(anode);
+                }
+
+                if (i == 1)
+                    node.left = anode;
+                else
+                    node.right = anode;
+            }
+        }
+
+        return root;
+    }
+
+    public static void printTree(TreeNode root){
+        if (root == null)
+            return;
+        System.out.print(root.val + "   ");
+        printTree(root.left);
+        printTree(root.right);
+    }
+}
