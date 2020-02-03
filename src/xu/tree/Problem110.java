42ab17dd4a997208e8dbe9caf6fe12c81865f64a
纬林 许 <xuweilin2014@outlook.com>
2020-02-03 16:54:11 +0800

    解决树问题

---
+++ src/xu/tree/Problem110.java
@@ -0,0 +1,33 @@
+package xu.tree;
+
+public class Problem110 {
+    private boolean isBalance = true;
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+
+        int depth = 0;
+        func(root, depth);
+        return isBalance;
+    }
+
+    private int func(TreeNode root, int depth) {
+        if (root == null)
+            return depth;
+
+        depth++;
+        int left = func(root.left, depth);
+        int right = func(root.right, depth);
+        if (left - right > 1 || right - left > 1){
+            isBalance = false;
+        }
+
+        return Math.max(left, right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = TreeUtil.buildTree(new Integer[]{3,9,20,null,null,15,7});
+        System.out.println(new Problem110().isBalanced(root));
+    }
+}
