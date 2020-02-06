715866898bf0dea3e3665ce17bbb11fb6d72248f
sssjody <2392824176@qq.com>
2020-02-06 20:26:07 +0800

    解决树问题

---
+++ src/xu/tree/Problem144.java
@@ -0,0 +1,13 @@
+package xu.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null)
+            return new ArrayList<>();
+
+        
+    }
+}
