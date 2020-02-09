c9d179a0081f47050f314e5ff4b8935dbac02ddf
sssjody <2392824176@qq.com>
2020-02-09 19:43:17 +0800

    解决回溯问题

--- src/xu/backtracking/Problem39.java
+++ src/xu/backtracking/Problem39.java
@@ -1,8 +1,6 @@
 package xu.backtracking;
 
-import java.util.ArrayList;
-import java.util.IllegalFormatCodePointException;
-import java.util.List;
+import java.util.*;
 
 public class Problem39 {
 
@@ -12,7 +10,8 @@ public class Problem39 {
         if (candidates.length == 0)
             return new ArrayList<>();
 
-        dfs(candidates, 0, target, 0, new ArrayList<>());
+        Arrays.sort(candidates);
+        dfs(candidates,0, target, 0, new ArrayList<>());
         return res;
     }
 
@@ -25,35 +24,22 @@ public class Problem39 {
         }
 
         for (int i = 0; i < candidates.length; i++) {
+            if (candidates[i] < val || candidates[i] > target)
+                continue;
             list.add(candidates[i]);
-            dfs(candidates,candidates[i], target, sum + candidates[i], list);
+            dfs(candidates, candidates[i], target, sum + candidates[i], list);
             list.remove(list.size() - 1);
         }
     }
 
     private void copyAndAdd(List<List<Integer>> res, List<Integer> list) {
-        boolean flag = false;
-        for (List<Integer> re : res) {
-            if (re.size() != list.size())
-                continue;
-            else{
-                for (int i = 0; i < re.size(); i++) {
-                    if (!re.contains(list.get(i)))
-                        flag = true;
-                }
-            }
-            if (!flag)
-        }
-
         List<Integer> vals = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            vals.add(list.get(i));
-        }
+        vals.addAll(list);
         res.add(vals);
     }
 
     public static void main(String[] args) {
-        List<List<Integer>> lists = new Problem39().combinationSum(new int[]{2, 3, 6, 7}, 7);
+        List<List<Integer>> lists = new Problem39().combinationSum(new int[]{2, 3, 5}, 8);
         for (List<Integer> list : lists) {
             System.out.println(list);
         }
