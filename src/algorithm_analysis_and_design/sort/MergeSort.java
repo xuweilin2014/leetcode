68e511732a2d9d2f8c6808c387a07c0947f16087
tom_glb <xuweilin2014@outlook.com>
2020-07-11 16:42:25 +0800

    归并排序

--- src/algorithm_analysis_and_design/sort/MergeSort.java
+++ src/algorithm_analysis_and_design/sort/MergeSort.java
@@ -2,11 +2,11 @@ package algorithm_analysis_and_design.sort;
 
 public class MergeSort {
 
-    public void mergeSort(int[] nums){
+    public static void mergeSort(int[] nums){
         MSort(nums, 0, nums.length - 1);
     }
 
-    private void MSort(int[] nums, int begin, int end) {
+    public static void MSort(int[] nums, int begin, int end){
         if (begin < end){
             int mid = (begin + end) / 2;
             MSort(nums, begin, mid);
@@ -15,19 +15,23 @@ public class MergeSort {
         }
     }
 
-    private void merge(int[] nums, int begin, int mid, int end) {
+    // 合并两个有序的子序列begin...mid和mid+1...end，使其变成一个子序列
+    private static void merge(int[] nums, int begin, int mid, int end) {
         int[] temp = new int[end - begin + 1];
-        int k = 0, i = begin, j = mid + 1;
+        int i = begin, j = mid + 1;
+        int k = 0;
         for (; i <= mid && j <= end;){
-            if (nums[i] < nums[j])
+            if (nums[i] <= nums[j]){
                 temp[k++] = nums[i++];
-            else
+            }else{
                 temp[k++] = nums[j++];
+            }
         }
 
         while (i <= mid){
             temp[k++] = nums[i++];
         }
+
         while (j <= end){
             temp[k++] = nums[j++];
         }
@@ -37,9 +41,10 @@ public class MergeSort {
         }
     }
 
+
     public static void main(String[] args) {
         int[] nums = new int[]{16,0,8,8,9,95,48,74,23,-8};
-        new MergeSort().mergeSort(nums);
+        mergeSort(nums);
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");
         }
