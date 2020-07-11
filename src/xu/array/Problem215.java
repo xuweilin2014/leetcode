708f81ef0f0ade9c1553c7dff34e4a3ae19cfb43
纬林 许 <xuweilin2014@outlook.com>
2020-07-12 00:10:10 +0800

    leetcode 215

---
+++ src/xu/array/Problem215.java
@@ -0,0 +1,17 @@
+package xu.array;
+
+import java.util.Arrays;
+
+public class Problem215 {
+
+    public static void main(String[] args) {
+        System.out.println(findKthLargest(new int[]{3,2,3,1,2,4,5,5,6}, 4));
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        return nums[nums.length - k];
+    }
+
+}
