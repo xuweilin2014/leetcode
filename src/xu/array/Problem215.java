f1c0d2e82357f857e943845c9ef4bdeee4d88faf
tom_glb <xuweilin2014@outlook.com>
2020-07-12 20:54:56 +0800

    215 数组中的第K个最大元素

--- src/xu/array/Problem215.java
+++ src/xu/array/Problem215.java
@@ -5,13 +5,52 @@ import java.util.Arrays;
 public class Problem215 {
 
     public static void main(String[] args) {
-        System.out.println(findKthLargest(new int[]{3,2,3,1,2,4,5,5,6}, 4));
+        System.out.println(findKthLargest(new int[]{-1,2,0}, 2));
     }
 
     public static int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
+        int index = doFind(nums, 0, nums.length - 1, k);
+        return nums[index];
+    }
 
-        return nums[nums.length - k];
+    public static int doFind(int[] nums, int left, int right, int k){
+        int p = 0, lp = 0, rp = 0;
+        p = partition(nums, left, right);
+        if (nums.length - p == k) {
+            return p;
+        } else if (nums.length - p < k) {
+            lp = doFind(nums, left, p - 1, k);
+        } else if (nums.length - p > k) {
+            rp = doFind(nums, p + 1, right, k);
+        }
+
+        if (nums.length - lp == k)
+            return lp;
+        else
+            return rp;
+    }
+
+    public static int partition(int[] nums, int left, int right){
+        int axis = nums[left];
+        int low = left;
+        int high = right;
+
+        while (low < high){
+            while (nums[high] >= axis && low < high)
+                high--;
+
+            if (low < high)
+                nums[low] = nums[high];
+
+            while (nums[low] <= axis && low < high)
+                low++;
+
+            if (low < high)
+                nums[high] = nums[low];
+        }
+        nums[high] = axis;
+
+        return high;
     }
 
 }
