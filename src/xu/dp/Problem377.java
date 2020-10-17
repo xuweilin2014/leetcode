6f7e499b3ef67c8c560941ea23fd7235b1478b79
tom_glb <xuweilin2014@outlook.com>
2020-10-17 12:08:33 +0800

    leetcode

---
+++ src/xu/dp/Problem377.java
@@ -0,0 +1,26 @@
+package xu.dp;
+
+import java.util.Arrays;
+
+public class Problem377 {
+
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        Arrays.sort(nums);
+        dp[0] = 1;
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] <= i)
+                    dp[i] += dp[i - nums[j]];
+            }
+        }
+
+        return dp[target];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Problem377().combinationSum4(new int[]{1, 2, 3}, 5));
+    }
+
+}
---
+++ src/xu/dp/Problem413.java
@@ -0,0 +1,31 @@
+package xu.dp;
+
+public class Problem413 {
+
+    public int numberOfArithmeticSlices(int[] A) {
+        if (A == null || A.length <= 2)
+            return 0;
+
+        int[] dp = new int[A.length];
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i < dp.length; i++) {
+            int prev = A[i - 1] - A[i - 2];
+            if (A[i] - A[i - 1] == prev){
+                dp[i] = dp[i - 1] - dp[i - 2] + 1 + dp[i - 1];
+            } else {
+                dp[i] = dp[i - 1];
+            }
+        }
+
+        return dp[dp.length - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Problem413().numberOfArithmeticSlices(new int[]{-1, 0, 1, 2, 5, 8, 11, 14, 17, 25}));
+        System.out.println(new Problem413().numberOfArithmeticSlices(new int[]{-1, 0, 1, 2}));
+        System.out.println(new Problem413().numberOfArithmeticSlices(new int[]{-4, 0, 1, 2,5,8,11,21}));
+    }
+
+}
