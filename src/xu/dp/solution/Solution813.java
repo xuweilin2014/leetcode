b5b3c16bc8f39391af2080c1ad33eaf11194661e
xuweilin2014 <xuweilin2014@outlook.com>
2020-12-29 12:23:57 +0800

    leetcode

--- src/xu/dp/solution/Solution813.java
+++ src/xu/dp/solution/Solution813.java
@@ -6,5 +6,6 @@ public class Solution813 {
      * 这道题目使用动态规划算法，令 dp[i][k] 表示前 i 个数分成 k 个数组的最大分数值。
      * dp[i][k] = max(dp[i][k], dp[j][k-1] + avg(j+1, i))
      * 其中，j < i，且 avg(j+1, i) 表示求 A[j+1] + A[j+2] + ... + A[i] 的和的平均值
+     * 碰到了之后不大可能现场写出来
      */
 }
