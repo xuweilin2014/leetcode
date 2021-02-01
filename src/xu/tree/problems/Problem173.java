552c484aaf5cdc9c97999c4b7963b6764c7a0e1b
xuweilin2014 <xuweilin2014@outlook.com>
2021-02-01 11:57:21 +0800

    leetcode

--- src/xu/tree/problems/Problem173.java
+++ src/xu/tree/problems/Problem173.java
@@ -3,65 +3,55 @@ package xu.tree.problems;
 import xu.tree.problems.TreeNode;
 import xu.tree.problems.TreeUtil;
 
+import java.util.Stack;
+
 /**
  * Your BSTIterator object will be instantiated and called as such:
  * BSTIterator obj = new BSTIterator(root);
  * int param_1 = obj.next();
  * boolean param_2 = obj.hasNext();
  */
-
 public class Problem173 {
-    private TreeNode index;
-    private int val = Integer.MIN_VALUE;
-    private boolean flag = false;
+
+    private Stack<TreeNode> stack = new Stack<>();
+
+    private TreeNode node;
 
     public Problem173(TreeNode root) {
-        this.index = root;
+        this.node = root;
     }
 
-    /** @return the next smallest number */
     public int next() {
-        flag = false;
-        func(index);
-        return val;
-    }
-
-    private void func(TreeNode root) {
-        if (root == null)
-            return;
-
-        func(root.left);
-        if (root.val > val && !flag){
-            val = root.val;
-            flag = true;
-            return;
+        if (node != null){
+            while (node != null){
+                stack.push(node);
+                node = node.left;
+            }
         }
-        func(root.right);
+        if (!stack.isEmpty()){
+            node = stack.pop();
+            TreeNode ans = node;
+            if (node.right != null) {
+                node = node.right;
+            }else {
+                node = null;
+            }
+            return ans.val;
+        }
+
+        return -1;
     }
 
-    /** @return whether we have a next smallest number */
     public boolean hasNext() {
-        flag = false;
-        helper(index);
-        return flag;
-    }
-
-    private void helper(TreeNode root) {
-        if (root == null)
-            return;
-
-        helper(root.left);
-        if (root.val > val && !flag){
-            flag = true;
-            return;
-        }
-        helper(root.right);
+        return !stack.isEmpty() || node != null;
     }
 
     public static void main(String[] args) {
-        TreeNode node = TreeUtil.buildTree(new Integer[]{7,3,15,null,null,9,20});
+        TreeNode node = TreeUtil.buildTree(new Integer[]{7,3,15,null,4,9,20});
         Problem173 iterator = new Problem173(node);
         System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.hasNext());
         System.out.println(iterator.next());
         System.out.println(iterator.hasNext());
         System.out.println(iterator.next());
--- src/xu/tree/problems/Problem450.java
+++ src/xu/tree/problems/Problem450.java
@@ -4,50 +4,54 @@ import xu.tree.problems.TreeNode;
 import xu.tree.problems.TreeUtil;
 
 public class Problem450 {
+
     public TreeNode deleteNode(TreeNode root, int key) {
-        return deleteBST(root, key);
+        if (root == null)
+            return null;
+
+        return func0(root, null, key);
     }
 
-    private TreeNode deleteBST(TreeNode cur, int key) {
-        if (cur == null)
-            return cur;
-        else if (cur.val > key) {
-            cur.left = deleteBST(cur.left, key);
-            return cur;
-        }
-        else if (cur.val < key) {
-            cur.right = deleteBST(cur.right, key);
-            return cur;
-        }
-        else
-            return delete(cur);
-    }
+    private TreeNode func0(TreeNode root, TreeNode parent, int key){
+        if (root == null)
+            return null;
 
-    private TreeNode delete(TreeNode cur) {
-        if (cur.right == null){
-            cur = cur.left;
-        }else if (cur.left == null){
-            cur = cur.right;
-        }else{
-            TreeNode s = cur.left, q = cur;
-            while (s.right != null){
-                q = s;
-                s = s.right;
-            }
-            cur.val = s.val;
-            if (q == cur){
-                cur.left = s.left;
-            }else{
-                q.right = s.left;
+        if (root.val > key)
+            root.left = func0(root.left, root, key);
+
+        if (root.val == key){
+            if (root.left == null && root.right == null)
+                return null;
+            else if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+            else {
+                TreeNode node = root.right;
+                TreeNode prev = root;
+                while (node.left != null) {
+                    prev = node;
+                    node = node.left;
+                }
+                root.val = node.val;
+                if (prev.right == node)
+                    prev.right = node.right;
+                else
+                    prev.left = node.right;
+                return root;
             }
         }
-        return cur;
+
+        if (root.val < key)
+            root.right = func0(root.right, root, key);
+
+        return root;
     }
 
     public static void main(String[] args) {
         TreeNode node = TreeUtil.buildTree(new Integer[]{5,3,6,2,4,null,7});
-        TreeNode newnode = new Problem450().deleteNode(node, 2);
-        TreeUtil.printTree(newnode);
+        TreeNode newNode = new Problem450().deleteNode(node, 5);
+        TreeUtil.printTree(newNode);
     }
 
 }
