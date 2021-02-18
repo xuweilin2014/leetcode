291a6bb87994eece0644051ec92837414bc073b9
xuweilin2014 <xuweilin2014@outlook.com>
2021-02-18 11:01:46 +0800

    leetcode 回溯

--- src/xu/backtracking/problems/Problem1079.java
+++ src/xu/backtracking/problems/Problem1079.java
@@ -1,63 +1,47 @@
 package xu.backtracking.problems;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Problem1079 {
 
-    private Set<String> list = new HashSet<>();
-    private int sum = 0;
+    private boolean[] visited;
+
+    private int ans = 0;
 
     public int numTilePossibilities(String tiles) {
-        return solutionFinal(tiles);
+        if (tiles == null || tiles.length() == 0)
+            return 0;
+
+        visited = new boolean[tiles.length()];
+        char[] chs = tiles.toCharArray();
+        Arrays.sort(chs);
+        func(chs, new StringBuilder(), 0);
+        return ans;
     }
 
-    private int solutionFinal(String tiles) {
-        int[] arr = new int[26];
-        char[] chars = tiles.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            arr[chars[i] - 'A']++;
-        }
-        dfs(arr);
-        return sum;
-    }
+    private void func(char[] chs, StringBuilder path, int layer){
+        if (layer > chs.length)
+            return;
 
-    private void dfs(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0)
+        if (!"".equals(path.toString()))
+            ans++;
+
+        for (int i = 0; i < chs.length; i++) {
+            if (visited[i])
+                continue;
+            if (i - 1 >= 0 && chs[i] == chs[i - 1] && !visited[i - 1])
                 continue;
 
-            sum++;
-            arr[i]--;
-            dfs(arr);
-            arr[i]++;
-        }
-    }
-
-    private int solution1(String tiles) {
-        char[] chars = tiles.toCharArray();
-        int[] visited = new int[tiles.length()];
-        func(chars, visited, 0, tiles.length(), "");
-        return list.size();
-    }
-
-    private void func(char[] chars, int[] visited, int index, int length, String path) {
-        if (index == length){
-            return;
-        }
-
-        for (int i = 0; i < chars.length; i++) {
-            String tmp = new StringBuilder().append(path).append(chars[i]).toString();
-            if (!list.contains(tmp) && visited[i] == 0){
-                list.add(tmp);
-                visited[i] = 1;
-                func(chars, visited, index + 1, length, tmp);
-                visited[i] = 0;
-            }
+            visited[i] = true;
+            path.append(chs[i]);
+            func(chs, path, layer + 1);
+            path.deleteCharAt(path.length() - 1);
+            visited[i] = false;
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(new Problem1079().numTilePossibilities("AABB"));
+        System.out.println(new Problem1079().numTilePossibilities("AAB"));
+        System.out.println(new Problem1079().numTilePossibilities("AAABBC"));
     }
 }
--- src/xu/backtracking/problems/Problem79.java
+++ src/xu/backtracking/problems/Problem79.java
@@ -4,51 +4,52 @@ package xu.backtracking.problems;
 public class Problem79 {
 
     private int[][] visited;
+    private boolean existed = false;
+    private int rows = 0;
+    private int cols = 0;
     private String word;
-    private int rows, cols;
-    private boolean flag = false;
+    private int[][] directions = new int[][]{{-1,0},{0,1},{1,0},{0,-1}};
 
     public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0)
+            return false;
+
         this.word = word;
-        this.rows = board.length;
-        this.cols = board[0].length;
+        rows = board.length;
+        cols = board[0].length;
         visited = new int[rows][cols];
-        char first = word.charAt(0);
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (board[i][j] == first){
-                    dfs(board, "", i, j, 0);
-                    if (flag)
-                        return true;
+                if (board[i][j] == word.charAt(0)){
+                    func(board, 0, i, j);
                 }
             }
         }
-        return false;
+
+        return existed;
     }
 
-    private void dfs(char[][] board, String path, int row, int col, int index) {
-        if (path.length() == word.length()){
-            flag = true;
-            return;
-        }
-
-        if (row < 0 || row >= rows || col < 0 || col >= cols
-                || visited[row][col] == 1 || path.length() > word.length())
+    private void func(char[][] board, int index, int row, int col) {
+        if (row < 0 || row >= rows || col < 0 || col >= cols || visited[row][col] == 1)
             return;
 
-        path += board[row][col];
-        if (path.charAt(index) != word.charAt(index))
+        if (existed)
             return;
 
-        if (!flag){
-            visited[row][col] = 1;
-            dfs(board, path, row, col + 1, index + 1);
-            dfs(board, path, row + 1, col, index + 1);
-            dfs(board, path, row, col - 1,  index + 1);
-            dfs(board, path, row - 1, col, index + 1);
-            visited[row][col] = 0;
+        if (board[row][col] == word.charAt(index)){
+            if (index == word.length() - 1){
+                existed = true;
+                return;
+            }
+            for (int[] direction : directions) {
+                visited[row][col] = 1;
+                func(board, index + 1, row + direction[0], col + direction[1]);
+                visited[row][col] = 0;
+            }
         }
     }
+
 
     public static void main(String[] args) {
         boolean see = new Problem79().exist(new char[][]{
@@ -56,6 +57,20 @@ public class Problem79 {
                 {'S','F','C','S'},
                 {'A','D','E','E'},
         }, "ABCCED");
+        System.out.println(see);
+
+        see = new Problem79().exist(new char[][]{
+                {'A','B','C','E'},
+                {'S','F','C','S'},
+                {'A','D','E','E'},
+        }, "SEE");
+        System.out.println(see);
+
+        see = new Problem79().exist(new char[][]{
+                {'A','B','C','E'},
+                {'S','F','C','S'},
+                {'A','D','E','E'},
+        }, "ABCB");
         System.out.println(see);
     }
 
