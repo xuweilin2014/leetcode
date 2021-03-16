b145b18c4d59c87ee072a0bf9b2f136690814788
xuweilin <xuweilin2014@outlook.com>
2021-03-16 15:26:00 +0800

    leetcode

--- src/offer/question31_40/questions/Question40.java
+++ src/offer/question31_40/questions/Question40.java
@@ -5,81 +5,55 @@ import java.util.Arrays;
 public class Question40 {
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Question40().getLeastNumbers(new int[]{0,0,2,3,2,1,1,2,0,4}, 1)));
         System.out.println(Arrays.toString(new Question40().getLeastNumbers(new int[]{0,0,2,3,2,1,1,2,0,4}, 10)));
     }
 
     // 使用快排的思想
-    public static int[] getLeastNumbers(int[] input, int k) {
-        k = Math.min(k, input.length);
-        int[] vals = new int[k];
-        partition(input, 0, input.length - 1, k);
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (k == 0 || arr == null || arr.length == 0)
+            return new int[]{};
+
+        func(arr, 0, arr.length - 1, k);
+        int[] ans = new int[k];
         for (int i = 0; i < k; i++) {
-            vals[i] = input[i];
+            ans[i] = arr[i];
         }
-        return vals;
+
+        return ans;
     }
 
-    private static void partition(int[] arr, int left, int right, int k){
-        if (left >= right)
+    private void func(int[] arr, int left, int right, int k) {
+        if (left > right)
             return;
 
-        int val = arr[left];
         int low = left;
         int high = right;
+        int key = arr[low];
         while (low < high){
-            while (low < high && arr[high] >= val)
+            while (low < high && arr[high] > key)
                 high--;
-            if (low < high)
+
+            if (low < high){
                 arr[low] = arr[high];
-
-            while (low < high && arr[low] <= val)
                 low++;
-            if (low < high)
+            }
+
+            while (low < high && arr[low] < key)
+                low++;
+
+            if (low < high){
                 arr[high] = arr[low];
-        }
-
-        arr[high] = val;
-        if (high == k) {
-        }
-        else if (high < k){
-            partition(arr, high + 1, right, k);
-        }else{
-            partition(arr, left, high - 1, k);
-        }
-    }
-
-    // 使用最大堆的思想
-    public int[] getLeastNumbers1(int[] arr, int k) {
-        if (k >= arr.length){
-            return arr;
-        }
-
-        int[] res = Arrays.copyOfRange(arr, 0, k);
-        for (int i = res.length / 2 - 1; i >= 0; i--) {
-            adjustHeap(res, i, k - 1);
-        }
-
-        for (int i = k; i < arr.length; i++) {
-            if (arr[i] < res[0]){
-                res[0] = arr[i];
-                adjustHeap(res, 0, k - 1);
+                high--;
             }
         }
+        arr[low] = key;
 
-        return res;
+        if (low > k)
+            func(arr, left, low - 1, k);
+        if (low < k)
+            func(arr, low + 1, right, k);
     }
 
-    public void adjustHeap(int[] arr, int i, int length){
-        int val = arr[i];
-        for (int j = 2 * i + 1; j <= length ; j = 2 * j + 1) {
-            if (j < length && arr[j] < arr[j + 1])
-                j++;
-            if (arr[j] < val)
-                break;
-            arr[i] = arr[j];
-            i = j;
-        }
-        arr[i] = val;
-    }
 
 }
