a7fa0c7a6a92be13f717d7c9f3976dbf1bad1b67
xuweilin <xuweilin2014@outlook.com>
2021-04-04 15:42:02 +0800

    剑指 offer

--- src/offer/question1_10/questions/Question3.java
+++ src/offer/question1_10/questions/Question3.java
@@ -3,25 +3,26 @@ package offer.question1_10.questions;
 public class Question3 {
 
     public static void main(String[] args) {
+        System.out.println(findRepeatNumber(new int[]{2, 1, 3, 1, 4}));
         System.out.println(findRepeatNumber(new int[]{2, 4, 1, 0, 2, 5, 3}));
     }
 
-    public static int findRepeatNumber(int[] nums) {
-        
-        int val = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            while (i != nums[i]){
-                val = nums[i];
-                if (val == nums[val])
-                    break;
-
-                nums[i] = nums[val];
-                nums[val] = val;
+    public static int findRepeatNumber(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            int val = numbers[i];
+            if (i != val){
+                if (numbers[val] == val)
+                    return val;
+                else {
+                    int tmp = numbers[i];
+                    numbers[i] = numbers[val];
+                    numbers[val] = tmp;
+                    i--;
+                }
             }
         }
-        
-        return val;
+
+        return -1;
     }
 
 }
--- src/offer/question1_10/questions/Question3_2.java
+++ src/offer/question1_10/questions/Question3_2.java
@@ -1,13 +1,6 @@
 package offer.question1_10.questions;
 
-/**
- * 在一个长度为 n+1 的数组中，所有的数字都在 1~n 范围内，所以数组中肯定有一个数字是重复的。请找出这个数字
- * 方案1：创建一个长度为 n+1 的辅助数组，然后逐一把原数组中的每个数字复制到辅助数组中，如果原数组中的元素的值为 m，那么把它
- * 复制到辅助数组中第 m 个元素上。这样就可以很方便地找出重复元素。
- *
- * 方案2：假设 1~n 的中点为 m，那么把原数组分为两部分：1~m，m+1~n，如果原数组中值在 1~m 这个范围内的元素个数大于 m 个，那么在这一半
- * 的区间中肯定存在重复元素，否则 m+1~n 这个区间中包含重复元素。然后继续把包含重复元素的区间一分为二，直到找到重复元素。
- */
+
 public class Question3_2 {
 
     public static void main(String[] args) {
@@ -15,32 +8,37 @@ public class Question3_2 {
     }
 
     public int getDuplication(int[] nums){
-        int start = 1;
-        int end = nums.length;
+        int high = nums.length - 1;
+        return func(nums, 1, high);
+    }
 
-        while (start < end){
-            int middle = (start + end) / 2;
-            int leftCount = rangeCount(start, middle, nums);
+    private int func(int[] nums, int low, int high) {
+        if (low == high - 1){
+            if (countOfRange(nums, low, low) > 1)
+                return low;
+            if (countOfRange(nums, high, high) > 1)
+                return high;
+        }
 
-            if (leftCount > (middle - start + 1)){
-                end = middle;
-            }else{
-                start = middle + 1;
+        int mid = (low + high) / 2;
+        int counter = countOfRange(nums, low, mid);
+        if (counter > (mid - low) + 1){
+            return func(nums, low, mid);
+        }else {
+            return func(nums, mid, high);
+        }
+    }
+
+    private int countOfRange(int[] nums, int start, int end){
+        int counter = 0;
+        for (int num : nums) {
+            if (num >= start && num <= end) {
+                counter++;
             }
         }
 
-        return start;
+        return counter;
     }
 
-    private int rangeCount(int start, int end, int[] nums) {
-        int count = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= start && nums[i] <= end)
-                count++;
-        }
-
-        return count;
-    }
 
 }
